9dc1366c56cd265d68548236f5a121d6982fb203
Ilya Vorobyev <vorbinn@list.ru>
2021-09-26 19:25:31 +0300

    Реализовано увеличение массива в классе стек и приоритетная очередь.

---
+++ src/Lesson_3/MyQueue.java
@@ -0,0 +1,63 @@
+package Lesson_3;
+
+public class MyQueue<T> {
+    private T[] list;
+    private int size;
+    private final int DEFAULT_CAPACITY = 10;
+    private int begin;
+    private int end;
+
+    public MyQueue(int capacity) {
+        if (capacity <= 0) {
+            throw new IllegalArgumentException("capacity: " + capacity);
+        }
+        list = (T[]) new Object[capacity];
+    }
+
+    public MyQueue() {
+        list = (T[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    private int nextIndex(int index) {
+        return (index + 1) % list.length;
+    }
+
+    public T peekFront() {
+        if (isEmpty()) {
+            throw new RuntimeException("Queue isEmpty");
+        }
+
+        return list[begin];
+    }
+
+    public void insert(T item) {
+        if (isFull()) {
+            //Расширение массива***
+            throw new RuntimeException("Queue isFull");
+        }
+        size++;
+        list[end] = item;
+        end = nextIndex(end);
+    }
+
+    public T remove() {
+        T temp = peekFront();
+        size--;
+        list[begin] = null;
+        begin = nextIndex(begin);
+        return temp;
+    }
+
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public boolean isFull() {
+        return size == list.length;
+    }
+}
