b1b52c96357083058a46ad58505742e7af811a5a
Ilya Vorobyev <vorbinn@list.ru>
2021-09-30 00:14:22 +0300

    Реализация очереди LinkedQueue используя класс MyLinkedList().

--- src/Lesson_4/Main.java
+++ src/Lesson_4/Main.java
@@ -3,6 +3,23 @@ package Lesson_4;
 public class Main {
     public static void main(String[] args) {
 
+        LinkedQueue<Integer> lq = new LinkedQueue<>();
+
+        lq.insert(5);
+        lq.insert(6);
+        lq.insert(78);
+        lq.insert(89);
+        lq.insert(101);
+
+        System.out.println(lq);
+        System.out.println("Удалили " + lq.remove());
+        System.out.println("Удалили " + lq.remove());
+
+        lq.insert(207);
+
+        System.out.println(lq);
+        System.out.println("Размер " + lq.size());
+        System.out.println("Первый элемент " + lq.peekFront());
 
     }
 }
